8fe8d5149f8dd0a44d1bcb9fb8b448613fffcb59
mojovsky <danielderewonko@gmail.com>
2024-07-31 18:06:55 +0100

    set up

--- server/todos.js
+++ server/todos.js
@@ -1,8 +1,9 @@
-import { database } from './database/database.js'
+import { database } from "./database/database.js";
 
 export function getAllTodos() {
-  // TODO: use a database query to get all todos
-  return []
+  const stmt = database.prepare("SELECT * FROM todos");
+  const todos = stmt.all();
+  return todos;
 }
 
 // TODO: add more functions to satisfy the what is needed in the index.js file
